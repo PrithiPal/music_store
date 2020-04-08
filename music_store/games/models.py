9eb6040872ba8213d51d927fcf5ce9c38ae0fea0
Prithi <pprithip@sfu.ca>
2020-04-07 19:54:49 -0700

    Added ratings color label in detail.html

--- music_store/games/models.py
+++ music_store/games/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Game(models.Model) : 
     name = models.CharField(max_length=20)
     image = models.ImageField(upload_to='')
-    about = models.CharField(max_length=100)
+    about = models.CharField(max_length=1000)
     rating = models.IntegerField(default=0)
     publisher = models.CharField(max_length=100,blank=True)
 
